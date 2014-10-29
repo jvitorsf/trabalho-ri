c5a7505299e847fcfb18e98cfd6c36bd5aa3cbbd
João Vitor Sozinho Ferreira <jvitorsf@gmail.com>
2014-10-28 23:49:06 -0200

    Duck Duck Go

--- server.js
+++ server.js
@@ -23,8 +23,11 @@ var BING_COSTUMER_ID = "6f229689-b777-4409-bc0d-5a1e68a689e0";
 var BING_CLIENT_SECRET = "N0bc6Sk75t8Nmzal3fR2Hw5Po9UZcdHX0slE0L0zPns=";
 
 var service_op  = "Web?$format=json&Query=";
-var rootUri = 'https://api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=';
+var bingUri = 'https://api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=';
 var auth    = new Buffer([ BING_ACCOUNT_KEY, BING_ACCOUNT_KEY ].join(':')).toString('base64');
+
+var duckUri = "http://api.duckduckgo.com/?q=";
+var duckAttr = "&format=json&pretty=1";
 
 var request = require('request').defaults({
   headers : {
@@ -44,8 +47,8 @@ var CONSUMER_SECRET = "ae5264ca12071cd2210080dd9c4439fcb59b2b35";
 var BING_ACCOUNT_KEY = "ni0yREhySwwi5aZNHUGRAfq7gk8F3XEylaezjilmfjg";
 var BING_COSTUMER_ID = "6f229689-b777-4409-bc0d-5a1e68a689e0";
 
-var YaBossClient = new YaBoss(CONSUMER_KEY, CONSUMER_SECRET);
-YaBossClient.search('web','yahoo', {count: 10}, function(err,dataFound,response){console.log(dataFound)});
+// var YaBossClient = new YaBoss(CONSUMER_KEY, CONSUMER_SECRET);
+// YaBossClient.search('web','yahoo', {count: 10}, function(err,dataFound,response){console.log(dataFound)});
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -55,20 +58,36 @@ app.use(bodyParser());
 	app.post('/bing', function(req, res) {
 		var query       = req.body.query;
 		request.get({
-		url : rootUri,
+		url : bingUri,
 		qs  : {
 			$format : 'json',
 			Query   : "'" + query + "'", // the single quotes are required!
 		}
 		}, function(err, response, body) {
-		if (err)
-			return res.send(500, err.message);
-		if (response.statusCode !== 200)
-			return res.send(500, response.body);
-		var results = JSON.parse(response.body);
-		res.send(results.d.results);
+			if (err)
+				return res.send(500, err.message);
+			if (response.statusCode !== 200)
+				return res.send(500, response.body);
+			var results = JSON.parse(response.body);
+			res.send(results.d.results);
+		});
 	});
-});
+
+	app.post('/duck', function(req, res) {
+		var query       = req.body.query;
+		var duckSearchQuery = duckUri + query + duckAttr;
+		console.log(duckSearchQuery);
+		request.get({
+		url : duckSearchQuery
+		}, function(err, response, body) {
+			if (err)
+				return res.send(500, err.message);
+			if (response.statusCode !== 200)
+				return res.send(500, response.body);
+			var results = JSON.parse(response.body);
+			res.send(results);
+		});
+	});
 
 var port 				= process.env.PORT || 8080;  // set our port
 var server_port 		= process.env.OPENSHIFT_NODEJS_PORT || 8080
